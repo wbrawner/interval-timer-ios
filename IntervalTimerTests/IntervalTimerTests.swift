dfa4780bd6fd9e3fc4d3a3c093443783aac5b26f
William Brawner <me@wbrawner.com>
2020-10-22 19:24:00 -0700

    Initial Commit

---
+++ IntervalTimerTests/IntervalTimerTests.swift
@@ -0,0 +1,34 @@
+//
+//  IntervalTimerTests.swift
+//  IntervalTimerTests
+//
+//  Created by William Brawner on 10/22/20.
+//  Copyright © 2020 William Brawner. All rights reserved.
+//
+
+import XCTest
+@testable import IntervalTimer
+
+class IntervalTimerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
